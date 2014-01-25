954be06b419b701586b273cec5b27a6ec2be8420
Hongbo He <hhbcarl@gmail.com>
2014-01-25 15:53:10 +0800

    add setup.py

---
+++ moon/lazy/__init__.py
@@ -0,0 +1,3 @@
+from .proxy import LazyProxy
+
+__all__ = ["LazyProxy"]
---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="moon",
+    version="0.2",
+    packages=["moon", "moon.sqlalchemy", "moon.lazy"],
+
+    install_requires=['sqlalchemy'],
+
+    description="Useful Snips All in One",
+    author="Hongbo He",
+    author_email="hhbcarl@gmail.com",
+    url="http://blog.graycarl.me",
+)
