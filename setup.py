6a721eef35e146cdc262794b86890a2d5778cb3d
Hongbo He <hhbcarl@gmail.com>
2014-04-26 11:33:06 +0800

    version to 0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="moon",
-    version="0.4",
+    version="0.5",
     packages=["moon", "moon.sqlalchemy", "moon.lazy", "moon.web"],
 
     install_requires=['sqlalchemy'],
